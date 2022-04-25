fd02aaeeb16fd6ef61614e9783797d435f30d4f7
Shobhit Sundriyal <shobhitsundriyal70@gmail.com>
2022-04-25 21:17:20 +0530

    responsive tweeks

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -164,7 +164,7 @@ function LandingPage() {
 				{/** flex */}
 				<div className='sm:flex container mt-20 mb-4'>
 					{/** left side */}
-					<div className='text-6xl font-rmsBold sm:w-[45%] leading-[110%]'>
+					<div className='text-4xl md:text-6xl font-rmsBold sm:w-[45%] leading-[110%]'>
 						Saving the
 						<br /> planet as simple as 1,2,3
 					</div>
@@ -195,7 +195,8 @@ function LandingPage() {
 					{arr.map((item, i) => (
 						<span className=' font-rmsBold' key={i}>
 							CNEGATIVE GENESIS &nbsp;
-							<span className=' text-lg'>X</span>&nbsp;&nbsp;
+							<span className=' text-sm sm:text-lg'>X</span>
+							&nbsp;&nbsp;
 						</span>
 					))}
 				</div>
@@ -203,9 +204,9 @@ function LandingPage() {
 
 			{/** Section 4 */}
 			<div className='container text-mirage'>
-				<div className='flex'>
+				<div className='md:flex'>
 					{/** left side picture */}
-					<div className='w-[49%] aspect-[10/11] bg-sate-500 flex items-center justify-center relative'>
+					<div className='w-[90%] mx-auto md:w-[49%] aspect-[10/11] bg-sate-500 flex items-center justify-center relative'>
 						<div className='absolute bg-light_pink h-[70%] w-[50%] rounded-3xl mr-[16%] mb-[2%] -z-10'></div>
 						<img
 							src={section4Image}
@@ -214,11 +215,11 @@ function LandingPage() {
 						/>
 					</div>
 					{/** right side info */}
-					<div className='w-[49%] flex flex-col items-center justify-evenly'>
-						<div className='text-6xl font-rmsBold ml-[8%] leading-[110%]'>
+					<div className='md:w-[49%] flex flex-col tems-center justify-evenly space-y-16'>
+						<div className='text-6xl font-rmsBold ml-[5%] md:ml-[8%] leading-[110%]'>
 							The Cnegative Genesis NFT
 						</div>
-						<div className='text-lg space-y-3'>
+						<div className='text-lg space-y-3 ml-[5%] md:ml-[8%]'>
 							<div className='flex items-center'>
 								<CheckCircleIcon className='h-8 text-lavender mr-[2%]' />
 								Be a part of the C-negative DAO{' '}
@@ -236,7 +237,7 @@ function LandingPage() {
 					</div>
 				</div>
 				{/** Our Partners */}
-				<div className=' font-rmsBold mt-10'>
+				<div className=' font-rmsBold mt-28 md:mt-10 mx-2'>
 					<div className='text-6xl text-center'>Our Partners</div>
 					<div className='flex bg-light_pink rounded-2xl p-8 mt-8 overflow-x-auto snap-x snap-mandatory'>
 						{/** left side */}
@@ -272,33 +273,33 @@ function LandingPage() {
 					</div>
 				</div>
 				{/** dropping Soon */}
-				<div className='flex items-center bg-lavender rounded-2xl mt-10 px-20 py-10'>
+				<div className='flex items-center bg-lavender rounded-2xl mt-10 px-5 sm:px-10 md:px-20 py-5 md:py-10 mx-2'>
 					<div className=' flex-grow flex flex-col space-y-4 font-rmsBold text-4xl'>
 						<div>
 							Dropping Soon on the
 							<br /> polygon network <br />
 						</div>
 						<a href={whitePaerLink} target='_blank'>
-							<div className='bg-white rounded-xl w-max text-base p-4 mt-4'>
+							<div className='bg-white rounded-xl w-max text-base px-4 py-3 md:py-4 mt-4'>
 								Read LitePaper
 							</div>
 						</a>
 					</div>
-					<div className='mr-24'>
-						<img src={dropping} />
+					<div className='mr-0 1016px:mr-24'>
+						<img src={dropping} className='h-28 sm:h-max' />
 					</div>
 				</div>
 			</div>
 
 			{/** faq section section 5 */}
 			<div className='bg-light_pink mt-16 relative py-20 overflow-hidden'>
-				<div className='container '>
+				<div className='container px-2'>
 					<FaqSection />
 				</div>
 				<div className=' bg-lavender w-max absolute z-20 -bottom-0 -left-48 animate-moveSide'>
 					{arr.map((item, i) => (
 						<span
-							className=' font-rmsBold text-black items-center inline-flex'
+							className=' font-rmsBold text-black text-sm sm:text-base items-center inline-flex'
 							key={i}
 						>
 							LET'S SAVE THE PLANET TOGETHER &nbsp;&nbsp;
@@ -311,29 +312,29 @@ function LandingPage() {
 
 			{/** section 6 last part */}
 			<div className='bg-dark_bg text-white relative'>
-				<div className='container flex flex-col items-center space-y-8 py-[5%] '>
-					<div className=' text-7xl text-center font-rmsBold max-w-3xl mx-auto'>
+				<div className='container px-8 sm:px-20 md:px-2 flex flex-col items-center space-y-8 py-[15%] sm:py-[10%] md:py-[5%]'>
+					<div className='text-3xl sm:text-5xl md:text-7xl text-center font-rmsBold max-w-3xl mx-auto'>
 						Buy green NFTs. Destroy Carbon Credits. Become a planet
 						hero.
 					</div>
 					<a
 						href={whitePaerLink}
 						target='_blank'
-						className='btn text-mirage p-4 font-rmsBold text-lg'
+						className='btn text-mirage p-2 sm:px-4 sm:py-3 md:py-4 font-rmsBold text-base md:text-lg'
 					>
 						Read LitePaper
 					</a>
 				</div>
 
-				<div className='container'>
+				<div className='container px-2'>
 					{/** join community */}
 					<div className='mt-auto bottom-0 flex items-center justify-between border-t-2 py-5 border-gray-700'>
 						{/** join */}
-						<div className=' space-y-4 '>
-							<div className='text-5xl font-rmsBold'>
+						<div className=' space-y-4 max-w-[80%]'>
+							<div className='text-3xl md:text-5xl font-rmsBold'>
 								Join the community
 							</div>
-							<div className='text-lg'>
+							<div className=' text-base md:text-lg'>
 								Get Early Access to Drops and have a say in the
 								development of the project by joining the
 								community
@@ -351,8 +352,12 @@ function LandingPage() {
 					</div>
 					{/** footer */}
 					<div className='flex py-5'>
-						<div className='flex-grow'>
-							<img src={logoWhite} />
+						<div className='flex-grow h-8'>
+							<img src={logoWhite} className='sm:block hidden' />
+							<img
+								src={logoOnlyWhite}
+								className='block sm:hidden h-8 w-8'
+							/>
 						</div>
 						<div className=' space-x-10'>
 							<a href=''>Homepage</a>
--- src/components/LandingPage/SetpsBubble.js
+++ src/components/LandingPage/SetpsBubble.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SetpsBubble({ title, description }) {
 	return (
-		<div className='bg-white rounded-2xl p-6 space-y-6'>
-			<div className='text-3xl font-rmsBold'>{title}</div>
+		<div className='bg-white rounded-2xl p-4 md:p-6 space-y-4 sm:space-y-6'>
+			<div className='text-xl md:text-3xl font-rmsBold'>{title}</div>
 
 			<div>{description}</div>
 		</div>
